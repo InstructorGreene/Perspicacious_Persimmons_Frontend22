addeed85418479dc7d505b6c81cdf937924d903a
Smita Pandya <60938493+smitax21@users.noreply.github.com>
2022-11-15 22:14:14 +0000

    Created the navbar and landing page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import HomePage from "./Components/Homepage/HomePage";
+import NavbarMenu from "./Components/Navbar/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarMenu />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
