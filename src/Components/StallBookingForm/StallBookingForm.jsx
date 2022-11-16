11da2fb2b5a7d48e8b75cebe1eaa505715af0b60
Smita Pandya <60938493+smitax21@users.noreply.github.com>
2022-11-16 14:26:43 +0000

    Changed the stallBookingForm form into react-hook-form

--- src/Components/StallBookingForm/StallBookingForm.jsx
+++ src/Components/StallBookingForm/StallBookingForm.jsx
@@ -1,39 +1,66 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 import "./StallBookingForm.css";
 
 function StallBookingForm() {
-  const [data, setData] = useState({
-    name: "",
-    businessName: "",
-    mobileNumber: "",
-    stallType: "",
-    comments: "",
-    status: "",
-    date: "",
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      name: "",
+      businessName: "",
+      mobileNumber: "",
+      stallType: "",
+      comments: "",
+      status: "",
+      date: "",
+    },
   });
 
   return (
     <>
-      <form className="booking-container">
+      <form className="booking-container" onSubmit={handleSubmit(console.log)}>
         <div className="booking-form-box">
           <div>
-            <input type="text" placeholder="Name" />
+            <input
+              type="text"
+              {...register("name", { required: true })}
+              placeholder="Name"
+            />
           </div>
           <div>
-            <input type="text" placeholder="Business Name" />
+            <input
+              type="text"
+              {...register("businessName", { required: true })}
+              placeholder="Business Name"
+            />
           </div>
           <div>
-            <input type="number" placeholder="Mobile Number" />
+            <input
+              type="number"
+              {...register("mobileNumber", { required: true })}
+              placeholder="Mobile Number"
+            />
           </div>
           <div>
-            <input type="text" placeholder="Stall Type" />
+            <input
+              type="text"
+              {...register("stallType", { required: true })}
+              placeholder="Stall Type"
+            />
           </div>
           <div>
-            <input type="textarea" placeholder="Comments" />
+            <input
+              type="textarea"
+              {...register("comments", { required: true })}
+              placeholder="Comments"
+            />
           </div>
           <div>
-            <input type="Date" placeholder="Date" />
+            <input
+              type="Date"
+              {...register("date", { required: true })}
+              placeholder="Date"
+            />
           </div>
           <Button className="add-button" variant="custom">
             Add Booking
