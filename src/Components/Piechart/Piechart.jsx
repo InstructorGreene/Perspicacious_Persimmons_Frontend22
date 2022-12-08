7bdb282fcaee3b2db8ddf75dbba1ae096c2de150
Hanna Svietik <hsvietik@gmail.com>
2022-12-08 21:27:05 +0000

    pie chart legend added

--- src/Components/Piechart/Piechart.jsx
+++ src/Components/Piechart/Piechart.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 import { PieChart } from "react-minimal-pie-chart";
 import "./Piechart.css";
 
 const Piechart = (props) => {
-  console.log(props)
+  console.log(props);
   const counter = (str) => {
     let type = str;
     return props.allBookings.reduce(
@@ -15,13 +15,8 @@ const Piechart = (props) => {
   const donationCount = counter("donation");
   const commercialCount = counter("commercial");
   const craftCount = counter("craft");
-  const total=foodCount + commercialCount + donationCount + craftCount
-  console.log(foodCount, commercialCount, donationCount, craftCount);
-  console.log(
-    "total stalls",
-    foodCount + commercialCount + donationCount + craftCount,
-    props.allBookings.length
-  );
+  const total = foodCount + commercialCount + donationCount + craftCount;
+
   return (
     <div className="pie">
       <PieChart
@@ -29,39 +24,55 @@ const Piechart = (props) => {
         lineWidth={75}
         segmentsShift={1}
         labelPosition={65}
-        viewBoxSize={[100,100]}
+        viewBoxSize={[100, 100]}
         labelStyle={(index) => ({
-          fontSize: '5px',
-          fontFamily: 'sans-serif',
+          fontSize: "5px",
+          fontFamily: "sans-serif",
         })}
         label={({ dataEntry }) => `${dataEntry.title} ${dataEntry.value}%`}
         data={[
           {
             title: "Donation",
-            value: Math.floor(donationCount/total*100),
+            value: Math.floor((donationCount / total) * 100),
             color: "lightblue",
           },
           {
             title: "Food",
-            value: Math.floor(foodCount/total*100),
+            value: Math.floor((foodCount / total) * 100),
             color: "lightgreen",
           },
           {
             title: "Commercial",
-            value: Math.floor(commercialCount/total*100),
+            value: Math.floor((commercialCount / total) * 100),
             color: "lightcoral",
           },
           {
             title: "Craft",
-            value: Math.floor(craftCount/total*100),
+            value: Math.floor((craftCount / total) * 100),
             color: "orange",
           },
         ]}
       />
-      ;
+      <div className="pie-legend">
+        <p>Total stalls: {total}</p>
+
+        <ul>
+          <li>
+            <span>Food stalls: {foodCount}</span>
+          </li>
+          <li>
+            <span>Donation stalls: {donationCount}</span>
+          </li>
+          <li>
+            <span>Commertial stalls: {commercialCount}</span>
+          </li>
+          <li>
+            <span>Craft stalls: {craftCount}</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
 
 export default Piechart;
-
